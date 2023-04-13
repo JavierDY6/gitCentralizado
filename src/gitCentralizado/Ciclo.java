a969ef364bac830d5b5a6ae2957b3308bc2e9e52
alumno <alumno@debian>
2023-04-13 08:56:44 +0200

    Version4 desarrolo

--- src/gitCentralizado/Ciclo.java
+++ src/gitCentralizado/Ciclo.java
@@ -5,5 +5,32 @@ public class Ciclo {
 	private String nombre;
 	private int duracion;
 	private String asignatura;
+	public int getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public int getDuracion() {
+		return duracion;
+	}
+	public void setDuracion(int duracion) {
+		this.duracion = duracion;
+	}
+	public String getAsignatura() {
+		return asignatura;
+	}
+	public void setAsignatura(String asignatura) {
+		this.asignatura = asignatura;
+	}
 
+	
+	
+	
 }
